e774bdc7ee6504cef06dec9d25ae8bc3bcef2edc
tugskh <tugsbillegkh@gmail.com>
2023-05-28 10:14:50 -0400

    cardViewImage, buttons added

---
+++ CardWorkout-Programmatic/RulesVC.swift
@@ -0,0 +1,37 @@
+//
+//  RulesVC.swift
+//  CardWorkout-Programmatic
+//
+//  Created by Tugs Khaliunbat on 5/28/23.
+//
+
+import UIKit
+
+class RulesVC: UIViewController {
+    
+    let titleLabel = UILabel()
+    let rulesLabel = UILabel()
+    let exerciseLabel = UILabel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        configureTitleLabel()
+    }
+    
+    func configureTitleLabel() {
+        view.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.text = "Rules"
+        titleLabel.font = .systemFont(ofSize: 40, weight: .bold)
+        titleLabel.textAlignment = .center
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 60),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30)
+        ])
+    }
+    
+
+}
